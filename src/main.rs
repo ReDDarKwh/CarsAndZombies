73c9d1a19d0f1bf14ed479f1078baa46236d4992
William Henry <turbowill4@gmail.com>
2024-09-29 13:39:03 -0400

    Add physics plugin

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 use bevy_flycam::prelude::*;
+use bevy_rapier3d::prelude::*;
 
 fn keyboard_input(
     keys: Res<ButtonInput<KeyCode>>,
@@ -24,12 +25,29 @@ fn setup(
     });
 }
 
+fn setup_physics(mut commands: Commands) {
+    /* Create the ground. */
+    commands
+        .spawn(Collider::cuboid(100.0, 0.1, 100.0))
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, -2.0, 0.0)));
+
+    /* Create the bouncing ball. */
+    commands
+        .spawn(RigidBody::Dynamic)
+        .insert(Collider::ball(0.5))
+        .insert(Restitution::coefficient(0.7))
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, 4.0, 0.0)));
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
+        .add_plugins(RapierDebugRenderPlugin::default())
         .add_plugins(PlayerPlugin)
         .insert_resource(ClearColor(Color::srgb(0.9, 0.3, 0.6)))
         .add_systems(Startup, setup)
+        .add_systems(Startup, setup_physics)
         .add_systems(Update, keyboard_input)
         .run();
 }
